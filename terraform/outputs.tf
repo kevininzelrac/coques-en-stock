bc605e0ce027beaad7b51c7d391fe18108cb6014
Kevin Inzelrac <kevininzelrac@me.com>
2024-04-17 09:41:19 +0200

    update terraform/outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,19 @@ output "stage_postgres_endpoint" {
 output "stage_lambda_oac_note" {
   value = module.stage.lambda_oac_note 
 }
+
+output "prod_cloudfront_url" {
+  value = module.prod.cloudfront_url
+}
+
+output "prod_name_servers" {
+  value = module.prod.name_servers
+}
+
+output "prod_postgres_endpoint" {
+  value = module.prod.postgres_endpoint
+}
+
+output "prod_lambda_oac_note" {
+  value = module.prod.lambda_oac_note 
+}
