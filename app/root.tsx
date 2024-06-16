6e65e46ad7ff92eab877d08649a192c56babb29a
Kevin Inzelrac <kevininzelrac@me.com>
2024-06-16 16:09:51 +0200

    update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -9,22 +9,19 @@ import {
   useLoaderData,
   useRouteError,
 } from "@remix-run/react";
-import { LoaderFunctionArgs } from "@remix-run/node";
+import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 
-import links from "./styles/index";
-export { links };
+import prisma from "./services/prisma.server";
+import withTryCatch from "./middlewares/withTryCatch";
+import withPriviledges from "./middlewares/withPriviledges";
 
 import Header from "./components/header";
 import Footer from "./components/footer";
 import auth from "./services/auth.server";
 import Nav from "./components/nav";
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const { id, firstname, email, avatar, headers } = await auth(request);
-  const user = id ? { id, firstname, email, avatar } : null;
-
-  return json({ user }, { headers });
-};
+import links from "./styles/index";
+export { links };
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -44,6 +41,76 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const { id, headers } = await auth(request);
+  const user = id
+    ? await prisma.user
+        .findUnique({
+          where: { id },
+          select: {
+            id: true,
+            role: true,
+            firstname: true,
+            email: true,
+            avatar: true,
+          },
+        })
+        .catch(() => null)
+    : null;
+
+  const menu = await prisma.menu
+    .findMany({
+      where: { post: withPriviledges(user, {}) },
+      select: { id: true, title: true },
+      orderBy: { index: "asc" },
+    })
+    .catch(() => null);
+
+  return json({ user, menu }, { headers });
+};
+
+export const action = async ({ request }: LoaderFunctionArgs) => {
+  const { id: userId } = await auth(request);
+  const body = await request.json();
+
+  if (request.method === "POST") {
+    if (body.action === "create") {
+      if (body.type === "page") {
+        const { category, title } = body;
+        const post = await withTryCatch(
+          prisma.post.create({
+            data: {
+              type: { connect: { title: "page" } },
+              category: { connect: { title: category } },
+              author: { connect: { id: userId } },
+              title: String(title),
+              content: "enter content here",
+            },
+            select: { id: true, title: true },
+          }),
+          "Failed to create post."
+        );
+        if (post.error) return json(post, { status: 500 });
+        return redirect(`/editor/page/${category}/${post.data.title}`);
+      }
+    }
+  }
+  return json(null, { status: 405, statusText: "Method Not Allowed" });
+};
+
+export default function App() {
+  const { user, menu } = useLoaderData<typeof loader>();
+
+  return (
+    <>
+      <Header />
+      <Nav user={user} menu={menu} />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
 export function ErrorBoundary() {
   const error = useRouteError();
   console.error("ERROR ", error);
@@ -56,7 +123,7 @@ export function ErrorBoundary() {
       </head>
       <body>
         <Header />
-        <Nav user={null} />
+        <Nav user={null} menu={null} />
         {isRouteErrorResponse(error) ? (
           error.status >= 400 && error.status <= 499 ? (
             <article data-warning>
@@ -87,18 +154,5 @@ export function ErrorBoundary() {
         <Scripts />
       </body>
     </html>
-  );
-}
-
-export default function App() {
-  const { user } = useLoaderData<typeof loader>();
-
-  return (
-    <>
-      <Header />
-      <Nav user={user} />
-      <Outlet />
-      <Footer />
-    </>
   );
 }
